744f7b7c9c9e0e0ed6c136857b074023ce43b77b
Divyansh Malhotra <divyansh.sgs@gmail.com>
2018-10-26 18:51:21 +0530

    Code to download file from firebase storage using its name from firebase database

---
+++ download.py
@@ -0,0 +1,18 @@
+import pyrebase
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "bageera2018.firebaseapp.com",
+  "databaseURL": "https://bageera2018.firebaseio.com",
+  "storageBucket": "bageera2018.appspot.com",
+  #"serviceAccount": "path/to/serviceAccountCredentials.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+#storage.child("example.jpeg").put("thumbDiv.jpeg")
+db = firebase.database()
+#db.child("users").set({1:"example.jpeg"})
+users = db.child("users").get()
+dlink=users.val()
+print(dlink[1])
+storage.child(dlink[1]).download("downloaded.jpg")
